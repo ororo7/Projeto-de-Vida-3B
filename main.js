2a7b1a2206a702af761719634e72743fbd945907
ororo7 <128612640+ororo7@users.noreply.github.com>
2024-05-06 07:41:36 -0300

    kjfgfwgh

--- main.js
+++ main.js
@@ -45,8 +45,16 @@ if (tempoFinal>0){
         return "PRAZO FINALIZADO";
     }
     }
-     
+    
 
-     function atualizaCronometro(){
-        for (let i=0; i < contadores.length; i++)
+    function atualizaCronometro(){
+        for (let i=0; i < contadores.length; i++){
+            contadores[i].textContent = calculaTempo(tempos[i]);  
         }
+    }
+    function comecaCronometro(){
+        atualizaCronometro();
+        setInterval(atualizaCronometro,1000);
+    }
+    comecaCronometro();
+    
